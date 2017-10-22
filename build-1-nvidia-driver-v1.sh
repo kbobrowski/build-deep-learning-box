c9ff28bd9a4b18af57135f64ac9577c462686e6e
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-23 01:52:13 +0200

    added second version of driver installation

---
+++ build-1-nvidia-driver-v1.sh
@@ -0,0 +1,4 @@
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
+
