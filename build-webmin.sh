913bd70581190b3ac28431ad0a17cb17aa4613eb
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-24 02:15:33 +0200

    not supported

--- build-webmin.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-add-repository -y "deb http://download.webmin.com/download/repository sarge contrib"
-wget -qO - http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
-sudo apt update
-sudo apt -y install apt-transport-https
-sudo apt -y install webmin
