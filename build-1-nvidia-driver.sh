a2c179ccf8e63b2023c6db45d97433887ec1fba6
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-24 14:36:38 +0200

    added xubuntu installation

--- build-1-nvidia-driver.sh
+++ build-1-nvidia-driver.sh
@@ -7,4 +7,3 @@ sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/
 sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
 sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
 
-sudo reboot now
---
+++ build-3-xubuntu.sh
@@ -0,0 +1,4 @@
+# minimal desktop environment
+sudo apt install -y xorg
+sudo apt install lightdm lightdm-gtk-greeter
+sudo apt install xubuntu-core
