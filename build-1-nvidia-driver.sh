836fd83befc66e529647c0a8e2807f76811b0c93
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2017-10-24 02:29:00 +0200

    small adjustments

--- build-1-nvidia-driver.sh
+++ build-1-nvidia-driver.sh
@@ -1,6 +1,10 @@
+# upgrade
 sudo apt update
 sudo apt -y upgrade
+
+# add nvidia repo and install driver
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
 sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
 
+sudo reboot now
